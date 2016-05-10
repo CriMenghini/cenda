1c325cfcab673e4717a4d529e9cf27106124139c
michelegentili93 <michele.gentili93@gmail.com>
2016-05-10 16:55:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ Created on Tue May 10 15:09:06 2016
 
 from library import *
 
+## prima domanda al prof.
+#tipo1 è una variabile booleana, true se dice che vuole solo random_graph(n,p false se vuole il regular_random_graph)
 tipo1 = 1==int(raw_input("Would you like to use: \n 1: Random Graph(n,p) \n 2: Regular Random Graph (n,m) \n [1/2]: "))
 
-
+#nodi
 n=int(raw_input('\nNumber of node? (Suggested less then 250, unless the computation of eigenvalues will be slow)\n'))
 
 
 if tipo1: 
+    #probabilità    
     p= float( raw_input('Probability p :'))
+    #crea il grafo random e lo associa a g    
     g=r_graph(n,p)
 else: 
+    #nuemro di edge per nodi    
     m= int(raw_input('Number of edges per node m :'))
+    #show è booleana, è true se vuole vedere l'evoluzione dei graphi    
     show= 1==int(raw_input('Do you want to see the evolution of the graph during the algorithm? \n 1:yes\n 2:no\n' ))
     g=r_graph_regular(n,m,show)
 
