080e2bd31d8e40ff4338b02e0ef4c49bd8f970ee
michelegentili93 <michele.gentili93@gmail.com>
2016-05-10 19:22:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,54 +27,24 @@ else:
     show= 1==int(raw_input('Do you want to see the evolution of the graph during the algorithm? \n 1:yes\n 2:no\n' ))
     g=r_graph_regular(n,m,show)
 
- #random graph: viene sempre connesso
-g = r_graph(100)
 
-#grafo esempio 
-G=nx.Graph()
 
-da = int(0)
-a = int(1)
-G.add_edge(da,a)
+#compute the adiacence matrix as a dict 
+Adj = {n : g.neighbors(n) for n in g.nodes()}
 
-da = int(1)
-a = int(2)
-G.add_edge(da,a)
-
-#se aggiungi questo
-'''da = int(2)
-a = int(3)
-G.add_edge(da,a)'''
-
-da = int(3)
-a = int(4)
-G.add_edge(da,a)
-
-#e questo viene un anello
-'''da = int(4)
-a = int(0)
-G.add_edge(da,a)'''
-
-#altrimenti è sconnesso
-
-#basta aggiungere un arco e viene connesso
-
-#stampo con metodo michele grafo esempio
-print_graph(G)
+print '\n \n Exemple of BFS on root = 2'
+print BFS(2,Adj)
 
 print "testing connectivity of graphs.."
 print ''
 
 print "algebraic method 1 (irreducibility)"
 print "is the random graph connected:", testConnectIrredA(g)
-print "is the example graph connected:", testConnectIrredA(G)
 print ''
 
 print "algebraic method 2 (eigenvalue of the Laplacianmatrix)"
 print "is the random graph connected:", testConnectLapEig(g)
-print "is the example graph connected:", testConnectLapEig(G)
 print ''
 
 print "breadth-first based algorithm"
 print "is the random graph connected:", testConnectBFS(g)
-print "is the example graph connected:", testConnectBFS(G)
